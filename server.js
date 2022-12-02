177f84c84a73760964d7a3bc661ed452c011615b
likelionSungGuk <sung9gi@gmail.com>
2022-12-02 22:25:35 +0900

    add: Graphql RESOLVERS

--- server.js
+++ server.js
@@ -3,20 +3,33 @@ import { ApolloServer, gql  } from "apollo-server"
 const typeDefs = gql`
   type User {
     id: ID
-    username: string
+    username: String
   }
   type Tweet {
     id: ID
-    text: string
+    text: String
     author: User
   }
   type Query {
     allTweets: [Tweet]
     tweet(id: ID): Tweet
   }
+  type Mutation {
+    postTweet(text: String, userId: ID!): Tweet
+    deleteTweet(id: ID!): Boolean
+  }
 `;
 
-const server = new ApolloServer({ typeDefs })
+const resolvers = {
+  Query: {
+    tweet() {
+      console.log('Tweet Query Called!')
+      return null
+    }
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
 server.listen().then(({url}) => {
   console.log(`running on url: ${url}`)
 })
